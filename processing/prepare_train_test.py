3ab3a74ddf2ed0318e1675959de5ff92c52b5077
Yang Xu <innerfirexy@gmail.com>
2023-03-24 14:40:38 -0700

    add compressing func

--- processing/prepare_train_test.py
+++ processing/prepare_train_test.py
@@ -12,10 +12,45 @@ parser.add_argument('--seed', type=int, default=3, help='the seed used for shuff
 
 
 def compress_gestures(input_dir, output_dir):
-    pass
+    """
+    input_dir: the directory containing the uncompressed gestures; can be the `output_dir` used by prepare_gesture()
+        Assuming there are train.txt and test.txt existing in this directory
+    output_dir: the directory to save the compressed gestures
+    """
+    input_file_train = os.path.join(input_dir, 'train.txt')
+    input_file_test = os.path.join(input_dir, 'test.txt')
+    assert os.path.exists(input_file_train)
+    assert os.path.exists(input_file_test)
+
+    output_file_train = os.path.join(output_dir, 'train.txt')
+    output_file_test = os.path.join(output_dir, 'test.txt')
+    compressed_lines_train = _compress_single_file(input_file_train)
+    compressed_lines_test = _compress_single_file(input_file_test)
+    with open(output_file_train, 'w') as f:
+        for line in compressed_lines_train:
+            f.write(line + '\n')
+    with open(output_file_test, 'w') as f:
+        for line in compressed_lines_test:
+            f.write(line + '\n')
+
+def _compress_single_file(input_file) -> List[str]:
+    compressed_lines = []
+    with open(input_file, 'r') as f:
+        for line in f:
+            items = line.strip().split()
+            compressed_items = []
+            for idx, item in enumerate(items):
+                if idx == 0:
+                    compressed_items.append(item)
+                elif item == compressed_items[-1]:
+                    continue
+                else:
+                    compressed_items.append(item)
+            compressed_lines.append(' '.join(compressed_items))
+    return compressed_lines
 
 
-def file_to_line(input_file) -> str:
+def _file_to_line(input_file) -> str:
     lines = []
     with open(input_file, 'r') as f:
         for line in f:
@@ -24,11 +59,11 @@ def file_to_line(input_file) -> str:
     return gestures
 
 
-def prepare_gesture(data_dir, output_dir, seed: int, test_ratio:float = 0.2):
+def prepare_train_test(data_dir, output_dir, seed: int, test_ratio:float = 0.2):
     data_files = [fname for fname in glob.glob(os.path.join(data_dir, '*.txt')) if ('train' not in fname and 'test' not in fname)]
     random.seed(seed)
     random.shuffle(data_files)
-    all_filelines = list(map(file_to_line, data_files))
+    all_filelines = list(map(_file_to_line, data_files))
     test_count = int(len(all_filelines) * test_ratio)
     test_data = all_filelines[:test_count]
     train_data = all_filelines[test_count:]
@@ -44,9 +79,13 @@ def prepare_gesture(data_dir, output_dir, seed: int, test_ratio:float = 0.2):
 def main(args):
     gesture_dir = '../data/gestures_by_id'
     gesture_output_dir = '../data/gesture'
+    compressed_gesture_output_dir = '../data/gesture_compressed'
     if not os.path.exists(gesture_output_dir):
         os.makedirs(gesture_output_dir)
-    prepare_gesture(gesture_dir, gesture_output_dir, seed=args.seed)
+    if not os.path.exists(compressed_gesture_output_dir):
+        os.makedirs(compressed_gesture_output_dir)
+    prepare_train_test(gesture_dir, gesture_output_dir, seed=args.seed)
+    compress_gestures(gesture_output_dir, compressed_gesture_output_dir)
 
 
 if __name__ == '__main__':
