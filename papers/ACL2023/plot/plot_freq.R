e97e1495d9f9bcc7e15f8aa15c9e8f6a2a171914
Yang Xu <innerfirexy@gmail.com>
2023-05-18 20:58:52 -0700

    add all plot code and data

---
+++ papers/ACL2023/plot/plot_freq.R
@@ -0,0 +1,111 @@
+require("data.table")
+require("ggplot2")
+
+setwd("plot")
+
+###
+# Uncompressed gestures
+data_raw <- fread("trm_gesture_gesture_ppl_per_token.txt")
+setkey(data_raw, gesture)
+data_raw$gesture <- as.character(data_raw$gesture)
+
+data_raw_summ <- data_raw[,
+  .(ppl_mean = mean(ppl), ppl_sd = sd(ppl), count = .N, ci_upper = mean_cl_boot(ppl)$ymax, ci_lower = mean_cl_boot(ppl)$ymin),
+  by=gesture]
+data_raw_summ <- data_raw_summ[order(-count)]
+# top 5
+# 31	63	2.96898	12.4056	42367	3.09417	2.86052
+# 32	64	6.05750	30.7523	20540	6.49515	5.65289
+# 30	56	6.25267	44.3266	20354	6.86402	5.66097
+# 1	    0	12.2483	86.9310	17003	13.4979	10.9667
+# 33	72	13.9893	75.6346	9264	15.5515	12.5498
+# 6	    36	51.5780	326.969	2762	64.6469	41.1306
+# 7	    32	70.4669	439.890	2038	91.4624	53.6883
+# 8	    24	142.672	688.943	1035	182.851	105.810
+# 9	    42	122.769	457.413	796	    163.085	95.5743
+# 10	40	212.412	858.591	718	    282.316	159.825
+sum(data_raw_summ$count)
+# 121540 ~ same as # of word tokens
+
+sorted_gestures <- data_raw_summ$gesture
+data_raw_summ$gesture <- factor(data_raw_summ$gesture, levels=sorted_gestures)
+
+p_raw <- ggplot(data_raw_summ[1:5,], aes(x=gesture, y=ppl_mean)) +
+  geom_linerange(aes(ymin=ci_lower, ymax=ci_upper))
+
+
+###
+# Compressed gesture
+data_comp <- fread("trm_gesture_gesture_compressed_ppl_per_token.txt")
+setkey(data_comp, gesture)
+data_comp$gesture <- as.character(data_comp$gesture)
+
+data_comp_summ <- data_comp[,
+  .(ppl_mean = mean(ppl), ppl_sd = sd(ppl), count = .N, ci_upper = mean_cl_boot(ppl)$ymax, ci_lower = mean_cl_boot(ppl)$ymin),
+  by=gesture]
+data_comp_summ <- data_comp_summ[order(-count)]
+
+head(data_comp_summ, 10)
+# gesture  ppl_mean     ppl_sd count  ci_upper  ci_lower
+# 1:      63  2.779571   2.861976  6430  2.849425  2.713113
+# 2:      56  4.248089   4.570749  5625  4.362672  4.128994
+# 3:      64  3.799365   3.543830  4944  3.892387  3.704802
+# 4:      72  7.308519  13.058217  3041  7.780707  6.869698
+# 5:      36 19.870917  32.763101  1164 21.800064 18.237990
+# 6:      32 29.033029  35.489065   984 31.417867 27.036412
+# 7:       0 57.985044  95.309595   482 67.389461 50.423464
+# 8:      24 67.330075  66.252950   431 74.038036 61.241462
+# 9:      42 87.113518  74.324703   409 94.280450 80.454942
+sum(data_comp_summ$count)
+# 26120 ~ smaller than raw count
+
+
+###
+# Frequency vs. Rank plot
+data_raw_summ$gesture <- factor(data_raw_summ$gesture, levels=as.character(data_raw_summ))
+data_raw_summ$rank <- seq(1:nrow(data_raw_summ))
+
+p_raw <- ggplot(data_raw_summ, aes(x=log(rank), y=log(count))) +
+  geom_col(fill="steelblue", alpha=0.5) +
+  #geom_point() +
+  geom_smooth(aes(group=1), se=FALSE, size = 1) +
+  # annotate("label", x=c(0, 0.693, 1.099), y=c(9.67, 9.13, 7.89),
+  #          label=c("<70>", "<69>", "<78>"), hjust="inward") +
+  theme_bw() +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), panel.grid.major = element_blank())
+
+
+data_comp_summ$gesture <- factor(data_comp_summ$gesture, levels=as.character(data_comp_summ))
+data_comp_summ$rank <- seq(1:nrow(data_comp_summ))
+
+p_comp <- ggplot(data_comp_summ, aes(x=log(rank), y=log(count))) +
+  geom_col(fill="#FC4E07", alpha=0.5) +
+  geom_smooth(aes(group=1), se=FALSE, color = "firebrick1", size = 1) +
+  # annotate("label", x=c(0, 0.693, 1.099), y=c(9.67, 9.13, 7.89),
+  #          label=c("<70>", "<69>", "<78>"), hjust="inward") +
+  theme_bw() +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(), panel.grid.major = element_blank())
+
+# Combined
+data_raw_summ$type <- "Raw"
+data_comp_summ$type <- "Compressed"
+data_freq <- rbindlist(list(
+    data_raw_summ[,.(rank, count, type)], data_comp_summ[,.(rank, count, type)]
+))
+# Gesture token old => new
+# 63 => 79
+# 56 => 70
+# 64 => 71
+p_freq <- ggplot(data_freq, aes(x=log(rank), y=log(count))) +
+  geom_col(aes(fill=type), alpha=0.5, position = position_dodge(), width=0.1) +
+  geom_smooth(aes(color=type, lty=type), se=FALSE, linewidth = 1.5) +
+  annotate("label", x=c(0, 0.693, 1.099), y=c(9.67, 9.13, 8.89),
+           label=c("<79>", "<70>", "<71>"), hjust="inward") +
+  scale_fill_manual("Gesture token type", values = c("#D55E00", "#0072B2")) + #E69F00 or #FC4E07
+  scale_color_manual("Gesture token type", values = c("#D55E00", "#0072B2")) + #0072B2 or #FC4E07
+  scale_linetype_discrete("Gesture token type") +
+  labs(x="Rank of gesture token (log scaled)", y="Frequency count (log scaled)") +
+  theme_bw() +
+  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank(),
+        panel.grid.major = element_blank(), legend.position = c(.8,.8))
+ggsave("freq_rank_log-log.pdf", plot = p_freq, width = 6, height = 4)
---
+++ papers/ACL2023/plot/plot_position.R
@@ -0,0 +1,178 @@
+require("data.table")
+require("ggplot2")
+
+setwd("plot")
+
+##
+# Compressed gestures
+data1 <- fread("trm_gesture_compressed_ppls_test.txt")
+setnames(data1, c("dialogID", "ppl", "position"))
+unique(data1$dialogID)
+#  [1]  0  1  2  3  4  5  6  7  8  9 10
+
+data2 <- fread("trm_gesture_compressed_ppls_train.txt")
+setnames(data2, c("dialogID", "ppl", "position"))
+unique(data2$dialogID)
+max(data2$dialogID) # 41
+
+data1[, dialogID := dialogID + max(data2$dialogID) + 1]
+unique(data1$dialogID)
+# [1] 42 43 44 45 46 47 48 49 50 51 52
+data1$dialogID <- as.integer(data1$dialogID)
+
+dt_compressed <- rbindlist(list(data1, data2))
+# p <- ggplot(dt_compressed, aes(x=position, y=ppl)) + geom_smooth()
+# ggsave("trm_gesture_compressed_ppl_all.pdf", plot = p, width = 5, height = 5)
+
+dt_comp_trm <- copy(dt_compressed)
+dt_comp_trm$type <- "compressed"
+
+
+##
+# Uncompressed gestures
+data1 <- fread("trm_gesture_ppls_test.txt")
+setnames(data1, c("dialogID", "ppl", "position"))
+unique(data1$dialogID)
+#  [1]  0  1  2  3  4  5  6  7  8  9
+
+data2 <- fread("trm_gesture_ppls_train.txt")
+setnames(data2, c("dialogID", "ppl", "position"))
+unique(data2$dialogID)
+max(data2$dialogID) # 42
+
+data_raw <- rbindlist(list(data1, data2))
+summary(data_raw$position)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max.
+# 0.00   16.00   33.00   43.22   57.00  265.00
+sd(data_raw$position)
+# 41.60697
+
+# p2 <- ggplot(data_raw, aes(x=position, y=ppl)) + geom_smooth()
+# ggsave("trm_gesture_ppl_all.pdf", plot = p2, width = 5, height = 5)
+
+dt_raw_trm <- copy(data_raw)
+dt_raw_trm$type <- "raw"
+
+dt_trm <- rbindlist(list(dt_comp_trm, dt_raw_trm))
+dt_trm$model <- "Transformer"
+
+
+####
+# LSTM data
+####
+# Compressed
+data1 <- fread("lstm_gesture_compressed_ppls_test.txt")
+setnames(data1, c("dialogID", "ppl", "position"))
+unique(data1$dialogID)
+
+data2 <- fread("lstm_gesture_compressed_ppls_train.txt")
+setnames(data2, c("dialogID", "ppl", "position"))
+unique(data2$dialogID)
+max(data2$dialogID) # 41
+
+data1[, dialogID := dialogID + max(data2$dialogID) + 1]
+unique(data1$dialogID)
+# [1] 42 43 44 45 46 47 48 49 50 51 52
+data1$dialogID <- as.integer(data1$dialogID)
+
+data_compressed <- rbindlist(list(data1, data2))
+# p <- ggplot(data_compressed, aes(x=position, y=ppl)) + geom_smooth()
+# ggsave("lstm_gesture_compressed_ppl_all.pdf", plot = p, width = 5, height = 5)
+
+dt_comp_lstm <- copy(data_compressed)
+dt_comp_lstm$type <- "compressed"
+
+
+# Uncompressed
+data1 <- fread("lstm_gesture_ppls_test.txt")
+setnames(data1, c("dialogID", "ppl", "position"))
+unique(data1$dialogID)
+
+data2 <- fread("lstm_gesture_ppls_train.txt")
+setnames(data2, c("dialogID", "ppl", "position"))
+unique(data2$dialogID)
+max(data2$dialogID) # 41
+
+data1[, dialogID := dialogID + max(data2$dialogID) + 1]
+unique(data1$dialogID)
+# [1] 42 43 44 45 46 47 48 49 50 51 52
+data1$dialogID <- as.integer(data1$dialogID)
+
+data_raw <- rbindlist(list(data1, data2))
+# p <- ggplot(data_raw, aes(x=position, y=ppl)) + geom_smooth()
+# ggsave("lstm_gesture_ppl_all.pdf", plot = p, width = 5, height = 5)
+
+dt_raw_lstm <- copy(data_raw)
+dt_raw_lstm$type <- "raw"
+
+dt_lstm <- rbindlist(list(dt_comp_lstm, dt_raw_lstm))
+dt_lstm$model <- "LSTM"
+
+# by gesture type
+ppls_raw <- rbindlist(list(dt_lstm[type=="raw"], dt_trm[type=="raw"]))
+ppls_comp <- rbindlist(list(dt_lstm[type=="compressed"], dt_trm[type=="compressed"]))
+
+###
+# Save and load
+fwrite(dt_trm, "ppls_trm.txt")
+fwrite(dt_lstm, "ppls_lstm.txt")
+fwrite(ppls_raw, "ppls_raw.txt")
+fwrite(ppls_comp, "ppls_comp.txt")
+
+ppls_trm <- fread("ppls_trm.txt")
+ppls_lstm <- fread("ppls_lstm.txt")
+ppls_raw <- fread("ppls_raw.txt")
+ppls_comp <- fread("ppls_comp.txt")
+
+###
+# Plot together
+
+plot_ppls_raw <- ggplot(ppls_raw[position<=100 & ppl<5 & ppl>1.2], aes(x=position, y=ppl)) +
+  geom_smooth(method="lm", aes(color=model, fill=model, lty=model)) +
+  geom_point(aes(color=model, shape=model), alpha=0.4, size=1) +
+  scale_color_brewer(palette = "Set1") + scale_fill_brewer(palette = "Set1") +
+  labs(x="Utterance position", y="Perplexity (local entropy)") +
+  theme_bw() + theme(legend.position = c(0.8, 0.9))
+ggsave("gesture_entropy_position.pdf", plot=plot_ppls_raw, width=5, height=5)
+
+
+plot_ppls_comp <- ggplot(ppls_comp[ppl<20], aes(x=position, y=ppl)) +
+  geom_smooth(method="gam", aes(color=model, fill=model, lty=model)) +
+  geom_point(aes(color=model, shape=model), alpha=0.4, size=1) +
+  scale_color_brewer(palette = "Set1") + scale_fill_brewer(palette = "Set1") +
+  labs(x="Utterance position", y="Perplexity (local entropy)") +
+  theme_bw() + theme(legend.position = c(0.8, 0.9))
+ggsave("gesture_compressed_entropy_position.pdf", plot=plot_ppls_comp, width=5, height=5)
+
+
+###
+# Compare Transformer vs. LSTM
+t.test(data_trm$ppl, data_lstm$ppl)
+
+t.test(data_trm[type=="raw"]$ppl, data_lstm[type=="raw"]$ppl)
+
+t.test(data_trm[type=="compressed"]$ppl, data_lstm[type=="compressed"]$ppl)
+
+###
+# Linear models for ppl vs. position increasing pattern
+# LSTM
+summary(lm(ppl ~ position, data = ppls_lstm[type=="raw" & position<=120]))
+#              Estimate Std. Error t value Pr(>|t|)
+# (Intercept) 2.3930072  0.0364196  65.707   <2e-16 ***
+# position    0.0016616  0.0008112   2.048   0.0406 *
+
+summary(lm(ppl ~ position, data = ppls_lstm[type=="compressed"]))
+# Estimate Std. Error t value Pr(>|t|)
+# (Intercept)  4.67015    0.18008  25.933  < 2e-16 ***
+# position     0.09661    0.01407   6.868 1.38e-11 ***
+
+# Tramsformer
+summary(lm(ppl ~ position, data = ppls_trm[type=="raw" & position<=120]))
+# Estimate Std. Error t value Pr(>|t|)
+# (Intercept) 2.507195   0.039102  64.119  < 2e-16 ***
+# position    0.002276   0.000871   2.613  0.00902 **
+
+summary(lm(ppl ~ position, data = ppls_trm[type=="compressed"]))
+# Estimate Std. Error t value Pr(>|t|)
+# (Intercept)  5.01426    0.19164  26.165  < 2e-16 ***
+# position     0.09302    0.01497   6.214  8.6e-10 ***
